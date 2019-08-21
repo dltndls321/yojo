8f6c3be21e5532ce962a1e8fc8c892fbe5122728
Lwinsun <lshenghae@gmail.com>
2019-08-21 12:39:37 +0900

    20190821 lwin commit

--- src/main/java/com/yogi/jogi/map/controller/MapController.java
+++ src/main/java/com/yogi/jogi/map/controller/MapController.java
@@ -3,10 +3,22 @@ package com.yogi.jogi.map.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
 @Controller
+@RequestMapping("map")
 public class MapController {
 	private static final Logger logger = LoggerFactory.getLogger(MapController.class);
+	ModelAndView mv = new ModelAndView();
+	@RequestMapping("test") //2. url에 board/test를 치면
+	public ModelAndView test() {
+		
+		mv.setViewName("maptest/maptest.do"); //3. board폴더아래의 test.jsp를 찾음
+		
+		return mv;
+	}
+	
 }
--- src/main/java/com/yogi/jogi/map/model/MapModel.java
+++ src/main/java/com/yogi/jogi/map/model/MapModel.java
@@ -2,4 +2,6 @@ package com.yogi.jogi.map.model;
 
 public class MapModel {
 
+	
+	
 }
