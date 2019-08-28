aef3c5be9e81cae17e5ee5e34823ad25b9cfad54
Lwinsun <lshenghae@gmail.com>
2019-08-28 17:19:57 +0900

    20190828 winsun map

--- src/main/java/com/yogi/jogi/map/dao/MapDao.java
+++ src/main/java/com/yogi/jogi/map/dao/MapDao.java
@@ -1,6 +1,7 @@
 package com.yogi.jogi.map.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ public class MapDao {
 		return sqlSession.selectList(Namespace+".getFoodList");
 	}
 	
-	public List<MapModel> getFoodList2(float startX, float startY, float endX, float endY) throws Exception {
-		return sqlSession.selectList(Namespace+".getFoodList2");
+	public List<MapModel> getFoodList2(Map map) throws Exception {
+		return sqlSession.selectList(Namespace+".getFoodList2",map);
 	}
 }
