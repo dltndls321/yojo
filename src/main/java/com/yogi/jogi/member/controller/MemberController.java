e67c719a91956589ca4ffbed5b1d589cb56694d0
dltndls321 <dltndls321@gmail.com>
2019-09-04 18:54:54 +0900

    commit

--- src/main/java/com/yogi/jogi/member/controller/MemberController.java
+++ src/main/java/com/yogi/jogi/member/controller/MemberController.java
@@ -216,7 +216,7 @@ public class MemberController {
 		boardModel.setMemNum((Integer) session.getAttribute("SessionMemberMemnum"));
 		List<FestReviewModel> reviewList = festReviewService.selectFestReviewWithMemNum(festReviewModel);
 		List<FestivalModel> festList = new ArrayList<FestivalModel>();
-		List<BoardModel> boardList = 
+		List<BoardModel> boardList = boardSerivce.selectBoardListWidhMemnum(boardModel);
 		int reviewListsize = reviewList.size();
 		int star = 0;
 		double avg=0;
@@ -266,6 +266,7 @@ public class MemberController {
 		model.addObject("spotReviewList",spotReviewList);
 		model.addObject("spotList",spotList);
 		model.addObject("spotreviewListsize",spotreviewListsize);
+		model.addObject("boardList",boardList);
 		System.out.println("memberprofile 최종");
 		model.setViewName("member/memberprofile.do");
 		return model;
